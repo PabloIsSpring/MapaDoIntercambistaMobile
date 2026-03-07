bc3f8b7316b717307bd40e26ffd0dcbcc23950bb
William <williamoliveira20040@gmail.com>
2026-03-07 17:57:29 -0300

    - Implementação do layout do menu superior com logo e busca - Implementação do carrossel de imagens utilizando ViewPager2 - Configuração inicial da troca automática dos banners - Estrutura inicial da interface da tela principal

---
+++ app/src/main/java/com/example/mapadointercambista/CarrosselAdapter.java
@@ -0,0 +1,52 @@
+package com.example.mapadointercambista;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CarrosselAdapter extends RecyclerView.Adapter<CarrosselAdapter.ViewHolder> {
+
+    private int[] imagens;
+
+    public CarrosselAdapter(int[] imagens) {
+        this.imagens = imagens;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.itens_carrossel, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        holder.imagem.setImageResource(imagens[position]);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return imagens.length;
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imagem;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            imagem = itemView.findViewById(R.id.imagemBanner);
+
+        }
+    }
+}
--- app/src/main/java/com/example/mapadointercambista/MainActivity.java
+++ app/src/main/java/com/example/mapadointercambista/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.mapadointercambista;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
+
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,73 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        if(savedInstanceState != null){
+            // Restaura os valores do estado salvo
+            int v1 = savedInstanceState.getInt("valor1");
+            int v2 = savedInstanceState.getInt("valor1");
+        }
+        else{
+            // Provavelmente inicializa as variáveis com valores
+        }
+
+        Log.d("Ciclo de vida", "onCreate()chamado");
+
+        // Carrossel
+        ViewPager2 carrossel = findViewById(R.id.carrossel);
+
+        int[] imagens = {
+                R.drawable.banner1,
+                R.drawable.banner2,
+                R.drawable.banner3
+        };
+
+        CarrosselAdapter adapter = new CarrosselAdapter(imagens);
+        carrossel.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d("Ciclo de vida", "onStart()chamado");
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.d("Ciclo de vida", "onRestart()chamado");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d("Ciclo de vida", "onResume()chamado");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d("Ciclo de vida", "onPause()chamado");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d("Ciclo de vida", "onStop()chamado");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d("Ciclo de vida", "onDestroy()chamado");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedIntanceState){
+        // Salva o estado atual do jogador
+        savedIntanceState.putInt("valor1", 1000); // Forma de declarar variável
+        savedIntanceState.putInt("valor2", 55);
+        //Invoca a super classe, para que seja possível salvar o estado
+        super.onSaveInstanceState(savedIntanceState);
     }
 }
